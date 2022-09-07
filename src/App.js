194300d3296d92224e56a0ec570d0ad7b0f18d59
tristan99 <ariescacy9@gmail.com>
2022-09-07 10:46:10 -0500

    commit s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import DrawerAppBar from './components/Navbar/Navbar';
+import ShowCards from './components/proyectCards/ShowCards';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <DrawerAppBar/>
+      <ShowCards/>
     </div>
   );
 }
